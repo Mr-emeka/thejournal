ed68fc9f27fa0ed9532ca8a5e7732cdfd9cb668b
mr-emeka <speak2c.emeka@gmail.com>
2026-03-23 01:25:31 -0700

    chore: create interface for wp post

---
+++ data/posts.type.ts
@@ -0,0 +1,36 @@
+// Raw WordPress GraphQL response types
+export interface WPImage {
+  sourceUrl: string;
+  altText: string;
+}
+
+export interface WPCategory {
+  id: string
+  name: string;
+  databaseId: number;
+}
+
+export interface WPPost {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  date: string;
+  content: string;
+  categories: {
+    nodes: WPCategory[];
+  };
+  featuredImage: {
+    node: WPImage;
+  } | null;
+}
+
+export interface WPPostEdge {
+  node: WPPost;
+}
+
+export interface WPPostsResponse {
+  posts: {
+    edges: WPPostEdge[];
+  };
+}
