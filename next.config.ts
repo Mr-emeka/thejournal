99e8a7a3d8873c2f13d75a5d354c801ecc3cf93a
mr-emeka <speak2c.emeka@gmail.com>
2026-03-23 01:23:45 -0700

    chore: allow local ip for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowLocalIP: true, // allows local ip addresses
+    remotePatterns: [
+      // They are both same way to allow localhost, but the second one is more specific and includes the port number
+      {
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8001",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
